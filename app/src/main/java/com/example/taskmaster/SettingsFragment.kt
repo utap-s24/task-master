4a6aa1f1f64418d7d4539ab2f1ef86afd611355a
Aribah Hoque <aribah.hoque@Aribahs-MacBook-Air.local>
2024-04-04 01:50:24 -0500

    started layout

---
+++ app/src/main/java/com/example/taskmaster/SettingsFragment.kt
@@ -0,0 +1,5 @@
+package com.example.taskmaster
+
+import androidx.fragment.app.Fragment
+class SettingsFragment: Fragment() {
+}
---
+++ app/src/main/java/com/example/taskmaster/TaskFragment.kt
@@ -0,0 +1,6 @@
+package com.example.taskmaster
+
+import androidx.fragment.app.Fragment
+
+class TaskFragment: Fragment() {
+}
