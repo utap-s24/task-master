df453d71f3b9fd0e927fcdc5c89b9f66501af699
kaylawan <kaylawan42@gmail.com>
2024-04-23 12:49:44 -0500

    starts

--- app/src/main/java/com/example/taskmaster/MainActivity.kt
+++ app/src/main/java/com/example/taskmaster/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.taskmaster
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Setting up window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Setting up the BottomNavigationView with NavController
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(navController)
     }
 }
+
